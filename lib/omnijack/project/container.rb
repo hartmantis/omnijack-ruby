8e578ae5ea77a03ddef492c640f43eb5f3e295ca
Jonathan Hartman <j@p4nt5.com>
2014-09-09 15:36:34 -0700

    Add subproject classes for each project

---
+++ lib/omnijack/project/container.rb
@@ -0,0 +1,43 @@
+# Encoding: UTF-8
+#
+# Author:: Jonathan Hartman (<j@p4nt5.com>)
+#
+# Copyright (C) 2014, Jonathan Hartman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require_relative '../project'
+
+class Omnijack
+  class Project
+    # A specialized class for the container project
+    #
+    # @author Jonathan Hartman <j@p4nt5.com>
+    class Container < Project
+      def initialize(**args)
+        super('container', **args)
+      end
+
+      #
+      # Override to prevent setting different projects
+      #
+      # @return [String]
+      #
+      def project(arg = nil)
+        set_or_return(:project,
+                      arg,
+                      equal_to: 'container')
+      end
+    end
+  end
+end
---
+++ spec/omnijack/project/chef_spec.rb
@@ -0,0 +1,56 @@
+# Encoding: UTF-8
+#
+# Author:: Jonathan Hartman (<j@p4nt5.com>)
+#
+# Copyright (C) 2014, Jonathan Hartman
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require_relative '../../spec_helper'
+require_relative '../../../lib/omnijack/project/chef'
+
+describe Omnijack::Project::Chef do
+  let(:obj) { described_class.new }
+
+  describe '#initialize' do
+    context 'no additional args' do
+      it 'initializes a chef project' do
+        res = obj
+        expect(res.project).to eq('chef')
+        expect(res.instance_variable_get(:@project)).to eq('chef')
+      end
+    end
+
+    context 'some additional args' do
+      let(:obj) { described_class.new(version: '6.6.6', prerelease: true) }
+
+      it 'passes those args on to the superclass' do
+        res = obj
+        expect(res.version).to eq('6.6.6')
+        expect(res.instance_variable_get(:@version)).to eq('6.6.6')
+        expect(res.prerelease).to eq(true)
+        expect(res.instance_variable_get(:@prerelease)).to eq(true)
+      end
+    end
+  end
+
+  describe '#project' do
+    it 'returns chef' do
+      expect(obj.project).to eq('chef')
+    end
+
+    it 'refuses attempts to override' do
+      expect { obj.project('bad') }.to raise_error
+    end
+  end
+end
